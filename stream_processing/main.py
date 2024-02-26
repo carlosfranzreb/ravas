cd7931d47ce4353560f88b6b9c56956bfc63760b
Carlos Franzreb <>
2024-02-26 16:27:14 +0100

    Move main to source folder

--- stream_processing/main.py
+++ stream_processing/main.py
@@ -20,12 +20,21 @@ def get_cls(cls_str: str):
     return getattr(module, cls_str)
 
 
-def main(config: dict) -> None:
+def main(config: dict, runtime: int = None) -> None:
     """
     - Create an logging directory, and store the config there.
     - Create a logging file in the logging directory.
     - Start the audio-video streamer with the given config.
+
+    Args:
+    - config: The base config.
+    - runtime: Stop the audio-video streamer after this time.
     """
+
+    # check if the config is valid
+    proc_size = config["audio"]["processing_size"]
+    buffer_size = config["audio"]["record_buffersize"]
+    assert proc_size > buffer_size, "Proc. size should be greater than buffer size"
 
     # create a logging directory and store the config
     log_dir = os.path.join(config["log_dir"], str(int(time.time())))
@@ -53,6 +62,7 @@ def main(config: dict) -> None:
         video_maximum_fps=config["video"]["maximum_fps"],
         audio_sampling_rate=config["audio"]["sampling_rate"],
         audio_processing_size=config["audio"]["processing_size"],
+        audio_record_buffersize=config["audio"]["record_buffersize"],
         audio_callback=cv_audio(**config["audio_callback"]),
         audio_pyaudio_input_device_index=config["audio"]["pyaudio_input_device_index"],
         use_video=config["video"]["use_video"],
@@ -67,10 +77,15 @@ def main(config: dict) -> None:
 
     audio_video_streamer.start()
     try:
-        while True:
-            time.sleep(1)
+        if runtime is not None:
+            time.sleep(runtime)
+            audio_video_streamer.stop()
+        else:
+            while True:
+                time.sleep(1)
     except KeyboardInterrupt:
         audio_video_streamer.stop()
+    log_listener.terminate()
 
 
 if __name__ == "__main__":
@@ -79,4 +94,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     with open(args.config, "r") as f:
         config = yaml.safe_load(f)
-    main(config)
+    main(config, runtime=50)
