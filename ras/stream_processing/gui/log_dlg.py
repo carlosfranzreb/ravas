a4485a49739c6df1a085a6ddd63184a4d7cd3733
russaa <>
2024-10-14 01:59:46 +0200

    feat(gui): add controls and shortcuts for clearing & copying all text for logging window

--- ras/stream_processing/gui/log_dlg.py
+++ ras/stream_processing/gui/log_dlg.py
@@ -1,7 +1,7 @@
 import logging
 
-from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QPlainTextEdit, QVBoxLayout
+from PyQt6.QtGui import QAction, QTextCursor
+from PyQt6.QtWidgets import QPlainTextEdit, QVBoxLayout, QToolBar
 
 from .settings_helper import RestorableDialog
 
@@ -20,17 +20,26 @@ class QTextEditLogger(logging.Handler):
         # print('calling QTextEditLogger.add(..)', flush=True)
         self.widget.appendPlainText(message)
 
+    def reset(self):
+        self.widget.clear()
+
+    def copyAll(self):
+        sels = self.widget.extraSelections()
+        self.widget.selectAll()
+        self.widget.copy()
+
+        # HACK deselect text (and move to end position):
+        cursor = self.widget.textCursor()
+        cursor.movePosition(QTextCursor.MoveOperation.End)
+        self.widget.setTextCursor(cursor)
+
+        self.widget.setExtraSelections(sels)
+
 
 class LogDialog(RestorableDialog):
     def __init__(self, parent):
         super().__init__(parent=parent)
         self.setWindowTitle('Logging')
-
-        actClose = QAction("Close", self)
-        actClose.setShortcut("Ctrl+L")  # NOTE use CTRL-L, same as for toggling log-window in MainWindow
-        actClose.setStatusTip("Close Logging Window")
-        actClose.triggered.connect(self.close)
-        self.addAction(actClose)
 
         logTextBox = QTextEditLogger(self)
         logTextBox.setFormatter(logging.Formatter(
@@ -39,7 +48,31 @@ class LogDialog(RestorableDialog):
         logging.getLogger().addHandler(logTextBox)
         self.logWidget = logTextBox
 
+        actClose = QAction("C&lose", self)
+        actClose.setShortcut("Ctrl+L")  # NOTE use CTRL-L, same as for toggling log-window in MainWindow
+        actClose.setToolTip("Close Logging Window")
+        actClose.triggered.connect(self.close)
+        self.addAction(actClose)
+
+        actReset = QAction("&Reset", self)
+        actReset.setShortcut("Ctrl+R")
+        actReset.setToolTip("Reset Logging Output")
+        actReset.triggered.connect(logTextBox.reset)
+        self.addAction(actReset)
+
+        actCopyAll = QAction("&Copy All", self)
+        actCopyAll.setShortcut("Ctrl+Shift+C")
+        actCopyAll.setToolTip("Copy All Logging Output to Clipboard")
+        actCopyAll.triggered.connect(logTextBox.copyAll)
+        self.addAction(actCopyAll)
+
+        tools = QToolBar()
+        tools.addAction(actReset)
+        tools.addAction(actCopyAll)
+        tools.addAction(actClose)
+
         layout = QVBoxLayout()
+        layout.addWidget(tools)
         layout.addWidget(logTextBox.widget)
         self.setLayout(layout)
 
