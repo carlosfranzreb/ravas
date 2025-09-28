087b46d28c61930d6365b33c398b83e22fbc78ec
Carlos Franzreb <>
2025-09-28 12:42:13 +0200

    Rename file

--- ras/stream_processing/models/mimivc/compile.py
+++ ras/stream_processing/models/mimivc/compile.py
@@ -16,79 +16,124 @@ from ...utils import resolve_file_path
 
 def compile_onnx():
     os.makedirs(resolve_file_path("onnx/"), exist_ok=True)
-    mimi = init_mimi()
+    mimi = init_mimi()[0]
     mimi.eval()
     mimi.requires_grad_(False)
+    batch_size = 1
 
-    # compile the mimi encoder
-    @torch.inference_mode()
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Input: float tensor of shape [T]
-        Output: float tensor of shape [C, T]
-        """
-        x = x.unsqueeze(0).unsqueeze(0)
-        x = self.conv_encoder(x)
-        x = self.transformer_encoder(x)[0]
-        x = self.downsample(x).squeeze(0).T
-        return x
+    x = torch.randn((1, 1, 1920), dtype=torch.float32)
+    for method in [
+        "encoder",
+        "encoder_transformer",
+        "downsample",
+        "upsample",
+        "decoder_transformer",
+        "decoder",
+    ]:
+        dump_file = resolve_file_path(f"onnx/mimi_{method}.onnx")
 
-    mimi_encoder = torch.nn.Module()
-    mimi_encoder.conv_encoder = mimi.encoder
-    mimi_encoder.transformer_encoder = mimi.encoder_transformer
-    mimi_encoder.downsample = mimi.downsample
-    mimi_encoder.forward = forward.__get__(mimi_encoder)
+        if "sample" not in method:
+            state = getattr(mimi, method)._init_streaming_state(batch_size)
 
-    encoder_in = torch.randn((1920), dtype=torch.float32)
-    torch.onnx.export(
-        mimi_encoder,
-        encoder_in,
-        resolve_file_path(f"onnx/mimi_encoder.onnx"),
-        input_names=["input"],
-        output_names=["output"],
-    )
+        if "transformer" in method:
+            input_names = ["input", "offsets", "layer_states"]
+            output_names = ["output", "new_offsets", "new_layer_states"]
+            input_tuple = (x, *state)
 
-    # compile the mimi decoder
-    @torch.inference_mode()
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Input: float tensor of shape [C, T]
-        Output: float tensor of shape [T]
-        """
-        x = self.upsample(x)
-        x = self.transformer_decoder(x)[0]
-        x = self.conv_decoder(x).squeeze()
-        return x
+            # Prepare inputs for ONNX model, used for testing after compilation
+            input_onnx = {"input": x.numpy(), "offsets": state[0].numpy()}
+            state_flat = flatten_state(state[1], key_prefix="layer_states_")
+            input_onnx.update({key: value.numpy() for key, value in state_flat.items()})
 
-    mimi_decoder = torch.nn.Module()
-    mimi_decoder.upsample = mimi.upsample
-    mimi_decoder.conv_decoder = mimi.decoder
-    mimi_decoder.transformer_decoder = mimi.decoder_transformer
-    mimi_decoder.forward = forward.__get__(mimi_decoder)
+        elif "sample" in method:
+            input_names = ["input"]
+            output_names = ["output"]
+            input_tuple = (x,)
 
-    decoder_in = torch.randn((1, 512, 1), dtype=torch.float32)
-    torch.onnx.export(
-        mimi_decoder,
-        decoder_in,
-        resolve_file_path(f"onnx/mimi_decoder.onnx"),
-        input_names=["input"],
-        output_names=["output"],
-    )
+            # Prepare inputs for ONNX model, used for testing after compilation
+            input_onnx = {"input": x.numpy()}
 
-    # test that the compiled models output the same values as the torch ones
-    onnx_encoder = ort.InferenceSession(resolve_file_path(f"onnx/mimi_encoder.onnx"))
-    onnx_decoder = ort.InferenceSession(resolve_file_path(f"onnx/mimi_decoder.onnx"))
+        else:  # seanet models
+            input_names = ["input", "state"]
+            output_names = ["output", "new_state"]
+            input_tuple = (x, state)
 
-    for model_name, model_torch, model_onnx, model_input in zip(
-        ["encoder", "decoder"],
-        [mimi_encoder, mimi_decoder],
-        [onnx_encoder, onnx_decoder],
-        [encoder_in, decoder_in],
-    ):
-        torch_out = model_torch(model_input)
-        onnx_out = model_onnx.run(["output"], {"input": model_input.numpy()})[0]
+            # Prepare inputs for ONNX model, used for testing after compilation
+            input_onnx = {"input": x.numpy()}
+            state_flat = flatten_state(state)
+            input_onnx.update({key: value.numpy() for key, value in state_flat.items()})
+
+        torch.onnx.export(
+            getattr(mimi, method),
+            input_tuple,
+            dump_file,
+            input_names=input_names,
+            output_names=output_names,
+            dynamo=True,
+        )
+
+        # test that the compiled models output the same values as the torch ones
+        model_onnx = ort.InferenceSession(dump_file)
+        torch_out = getattr(mimi, method)(*input_tuple)
+        torch_out = torch_out[0]
+        if isinstance(torch_out, list):
+            torch_out = torch_out[0]
+
+        # Run ONNX model
+        onnx_out = model_onnx.run(output_names, input_onnx)
+        onnx_out = onnx_out[0]
         onnx_out = torch.from_numpy(onnx_out)
-        print(model_name, torch.allclose(torch_out, onnx_out, atol=1e-6))
+        print(method, torch.allclose(torch_out, onnx_out, atol=1e-4))
+
+        # update input for next module
+        x = torch_out
+        if method == "downsample":
+            x = mimi.quantizer.encode(x.unsqueeze(0))
+            x = mimi.quantizer.decode(x)
+        elif method == "upsample":
+            x = x.unsqueeze(0)
+
+
+def flatten_state(
+    state: list, key_prefix: str = "states_", master: bool = True
+) -> dict:
+    """
+    Flatten the state into a dictionary, as expected by the ONNX compiler.
+
+    Input elements that are tensors are called 'states_{idx}', where `idx` refers to the
+    element's list index. For input elements that are lists, its inputs are enumerated
+    and called 'states_{idx}_{idx2}', where `idx2` is the index of the sub-element in
+    its parent list. This is implemented recursively for arbitrary depths.
+
+    The ONNX compiler expects the first element to be called like the original argument
+    if it is a tensor. This is the only exception to the recursion explained above.
+    """
+    out = dict()
+    for elem_idx, elem in enumerate(state):
+        if isinstance(elem, Tensor):
+            out[f"{key_prefix}{elem_idx}"] = elem
+        elif isinstance(elem, list):
+            out.update(
+                flatten_state(elem, key_prefix=f"{key_prefix}{elem_idx}_", master=False)
+            )
+        else:
+            raise ValueError("Invalid element type")
+
+    # fix first key naming
+    if master:
+        for key in out:
+            if key.endswith("_0") and not any(c in "123456789" for c in key):
+                first_value = out[key]
+                break
+
+        del out[key]
+
+        if key.startswith("layer_states"):
+            out["layer_states"] = first_value
+        else:  # seanet
+            out["state"] = first_value
+
+    return out
 
 
 if __name__ == "__main__":
