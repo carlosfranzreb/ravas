efe564fbc24d48ed5df3f9c05eda53a3e3a6a0c2
Carlos Franzreb <>
2025-11-03 12:00:14 +0100

    Delete react app

--- rpm/run_docker_build_extension_rpm.sh
+++
@@ -1,3 +0,0 @@
-
-cd ..
-docker compose run -v "$(pwd)"/rpm/src:/app/src -v "$(pwd)"/rpm/public:/app/public -v "$(pwd)"/rpm/build:/app/build -v "$(pwd)"/rpm/resources:/app/resources "$(pwd)"/rpm/dist:/app/dist --entrypoint 'npm run build:extension' --rm rpm
--- rpm/run_docker_build_rpm.sh
+++
@@ -1,3 +0,0 @@
-
-cd ..
-docker compose run -v "$(pwd)"/rpm/src:/app/src -v "$(pwd)"/rpm/public:/app/public -v "$(pwd)"/rpm/build:/app/build --entrypoint 'npm run build' --rm rpm
