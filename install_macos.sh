1d430b6aa2c9d04d8dde9086e5aabac037a11a2c
Carlos Franzreb <>
2025-11-03 14:13:28 +0100

    Compile Mimi

--- install_macos.sh
+++ install_macos.sh
@@ -53,17 +53,32 @@ else
     fi
 fi
 
+# create conda environment and install dependencies
 conda create -p ./venv python=3.10 -y
+conda init bash
+conda init zsh
 conda activate ./venv
 cd ravas
 pip install .
 
 
+# prepare chrome extension for the avatar renderer
 cd ../rpm
 ensure_chrome_installed || { echo "Could not ensure Google Chrome is installed" >&2; exit 1; }
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --pack-extension=resources/dist/chrome-extension --pack-extension-key=resources/chrome-extension-packing/privkey.pem 
 mkdir resources/dist
 mv resources/chrome-extension.crx resources/dist/chrome-extension.crx
 
+# compile mimi models
 cd ../ravas
+mkdir onnx
+python -m stream_processing.models.mimivc.compile
+
+# download Mimi target features
+mkdir target_feats
+cd target_feats
+wget https://github.com/carlosfranzreb/ravas/releases/download/v0.7/1089.pt
+cd ..
+
+# run GUI
 python -m run_gui
